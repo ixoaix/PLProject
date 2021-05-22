249b2bed4561337787c18f8b227181f5b6fa9932
lhm <wakkkka@sjtu.edu.cn>
2021-05-22 22:43:08 +0800

    0

---
+++ F0503_Discrete_Fourier_Transform.v
@@ -0,0 +1,72 @@
+(** Discrete Fourier transformation can be used to compute big number's
+    multiplication. FFT is a well-known efficient algorithm to calculate
+    discrete fourier transformation. Your should learn how this algorithm works
+    and prove its correctness in Coq. You need to either define this algorithm
+    using complex numbers (you may find some library in Coq's standard library
+    or other Coq's user's contribution) or via an abstract ring with some extra
+    properties.
+
+    You only need to define this algorithm as a Coq function or a Coq relation
+    (small step description of this algorithm). You do not need to implement it
+    in a programming language like C. You do not need to verify its 
+    implementation in any programming language. *)
+
+(* 2021-05-07 20:39 *)
+From Coq Require Import Reals ssreflect.
+
+
+Definition C := (R * R)%type.
+
+(** *** Arithmetic operations *)
+
+Definition Cplus (x y : C) : C := (Rplus (fst x) (fst y), Rplus (snd x) (snd y)).
+Definition Copp (x : C) : C := (Ropp(fst x), Ropp( -snd x)).
+Definition Cminus (x y : C) : C := Cplus x (Copp y).
+Definition Cmult (x y : C) : C := (Rminus (Rmult (fst x) (fst y))  (Rmult (snd x) (snd y)), Rplus (Rmult(fst x) (snd y)) (Rmult(snd x) (fst y))).
+Definition Cinv (x : C) : C := (Rdiv (fst x) (Rplus (fst x ^ 2) (snd x ^ 2)), Ropp (Rdiv (snd x) (fst x ^ 2 + snd x ^ 2))).
+Definition Cdiv (x y : C) : C := Cmult x (Cinv y).
+
+Declare Scope C_scope.
+
+Local Open Scope C_scope.
+Delimit Scope C_scope with C.
+Infix "+" := Cplus : C_scope.
+Notation "- x" := (Copp x) : C_scope.
+Infix "-" := Cminus : C_scope.
+Infix "*" := Cmult : C_scope.
+Notation "/ x" := (Cinv x) : C_scope.
+Infix "/" := Cdiv : C_scope.
+
+Notation "(x, y)" := C : C_scope.
+
+Notation "[ ]" := nil (format "[ ]") : list_scope.
+Notation "[ x ]" := (cons x nil) : list_scope.
+Notation "[ x ; y ; .. ; z ]" := (cons x (cons y .. (cons z nil) ..)) : list_scope.
+Infix "::" := cons (at level 60, right associativity) : list_scope.
+Infix "++" := app (right associativity, at level 60) : list_scope.
+
+Fixpoint EvenList (l:list C) : list C := 
+  match l with
+     | nil => nil
+     | (x :: l' )%list=> x :: OddList l'
+end
+with OddList (l:list C) : list C := 
+  match l with
+     | nil => nil
+     | (_ :: l' )%list=> EvenList l'
+    end.
+
+Lemma example1: forall (x y z w : C) , EvenList [x; y; z; w] = [x; z]%list.
+Proof.
+  intros.
+  simpl.
+  reflexivity.
+Qed.
+
+Lemma example2: forall (x y z : C) , OddList [x; y; z] = [y]%list.
+Proof.
+  intros.
+  simpl.
+  reflexivity.
+Qed.
+
