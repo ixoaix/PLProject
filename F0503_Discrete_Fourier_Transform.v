6ffe8f4f700b8ed1f40540b1056011bb5ad6c09e
ixoaix <84632039+ixoaix@users.noreply.github.com>
2021-05-24 00:38:37 +0800

    0

--- F0503_Discrete_Fourier_Transform.v
+++ F0503_Discrete_Fourier_Transform.v
@@ -13,7 +13,7 @@
 
 (* 2021-05-07 20:39 *)
 From Coq Require Import Reals ssreflect.
-
+Require Import PL.Imp.
 
 Definition C := (R * R)%type.
 
@@ -56,6 +56,76 @@ with OddList (l:list C) : list C :=
      | (_ :: l' )%list=> EvenList l'
     end.
 
+Print pow_2_n.
+(* pow_2_n = fun n : nat => (2 ^ n)%R
+     : nat -> R *)
+Print Zpower.two_power_nat.
+(* Zpower.two_power_nat = 
+fun n : nat => Z.pos (Zpower.shift_nat n 1)
+     : nat -> Z *)
+Print Init.Nat.pow.
+(* Init.Nat.pow = 
+fix pow (n m : nat) {struct m} : nat :=
+  match m with
+  | 0 => 1
+  | S m0 => (n * pow n m0)%nat
+  end
+     : nat -> nat -> nat *)
+Print Nat.pow.
+(* Nat.pow = 
+fix pow (n m : nat) {struct m} : nat :=
+  match m with
+  | 0 => 1
+  | S m0 => (n * pow n m0)%nat
+  end
+     : nat -> nat -> nat *)
+Print INR.
+(* INR = 
+fix INR (n : nat) : R :=
+  match n with
+  | 0 => 0%R
+  | 1 => 1%R
+  | S (S _ as n0) => (INR n0 + 1)%R
+  end
+     : nat -> R *)
+Print Int_part.
+(* Int_part = fun r : R => (up r - 1)%Z
+     : R -> Z *)
+Print Z.of_nat.
+(* Z.of_nat = 
+fun n : nat =>
+match n with
+| 0 => 0%Z
+| S n0 => Z.pos (Pos.of_succ_nat n0)
+end
+     : nat -> Z *)
+Print Datatypes.length.
+(* length: forall [A : Type], list A -> nat *)
+Print Z.pow.
+(* Z.pow = 
+fun x y : Z =>
+match y with
+| 0 => 1
+| Z.pos p => Z.pow_pos x p
+| Z.neg _ => 0
+end
+     : Z -> Z -> Z *)
+
+Definition exp_complex (x : nat) : C :=
+  (cos_n x, sin_n x).
+
+Definition lenX_pow_2_n_1 (X : list C) : Prop :=
+  exists n, Z.of_nat (Datatypes.length X) = Zpower.two_power_nat n.
+(* equal of Z *)
+
+Definition lenX_pow_2_n_2 (X : list C) : Prop :=
+  exists n, Datatypes.length X = Init.Nat.pow 2 n.
+(* equal of nat *)
+
+Definition lenX_pow_2_n_3 (X : list C) : Prop :=
+  exists n, Z.of_nat (Datatypes.length X) = Z.pow 2 n.
+(* equal of Z *)
+
 Lemma example1: forall (x y z w : C) , EvenList [x; y; z; w] = [x; z]%list.
 Proof.
   intros.
@@ -69,3 +139,37 @@ Proof.
   simpl.
   reflexivity.
 Qed.
+
+Lemma example3: forall (x y z w : C) , lenX_pow_2_n_1 [x; y; z; w].
+Proof.
+  intros.
+  unfold lenX_pow_2_n_1.
+  exists 2%nat.
+  unfold Zpower.two_power_nat.
+  simpl.
+reflexivity.
+Qed.
+
+Lemma example4: forall (x y z w : C) , lenX_pow_2_n_2 [x; y; z; w].
+Proof.
+  intros.
+  unfold lenX_pow_2_n_2.
+  exists 2%nat.
+  simpl.
+  reflexivity.
+Qed.
+
+Lemma example5: forall (x y z w : C) , lenX_pow_2_n_3 [x; y; z; w].
+Proof.
+  intros.
+  unfold lenX_pow_2_n_3.
+  exists 2.
+  simpl.
+  reflexivity.
+Qed.
+
+Fixpoint X_len2pow (X : list C) : list C :=
+  match (lenX_pow_2_n_3 X) with
+    | True => X
+    | False => X_len2pow (X ++ (0, 0)::nil)
+  end.
